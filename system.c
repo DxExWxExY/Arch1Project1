90d730b9762b223fa85d255a69584f29ff5f2050
student <student@localhost.localdomain>
2017-10-05 17:51:29 -0600

    Initial Commit

---
+++ system.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+  /*Here the methods in charge of creating the list will be called*/
+  
+}
+
+struct Employee {
+  char name[40];
+  char lastName[40];
+  Employee next;
+};
+
+Employee databseBuilder() {
+
+}
